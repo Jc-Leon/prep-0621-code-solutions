94ca53ecb4e712449fbb38da1917fc43e5e387e9
Jc Leon <leonjuan123@yahoo.com>
2021-06-16 21:56:58 -0700

    completed prep-javascript-functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -1,6 +1,56 @@
-function addTwoNumbers(x, y) {
-  return x + y;
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
 }
+var sum = addTwoNumbers(2, 2);
+console.log(sum);
 
-var sum = addTwoNumbers(4, 4);
-console.log('sum', sum);
+
+function convertHourstoMinute(hours) {
+ return Math.floor(hours * 60);
+}
+var minutes = convertHourstoMinute(2);
+console.log(minutes)
+
+
+function getGreeting (name) {
+  return "Hello" + name;
+}
+console.log(getGreeting(' World!'))
+
+
+function addAndMultiplyBy5 (num1, num2) {
+  return (num1 + num2) * 5 ;
+}
+var sum = addAndMultiplyBy5(10, 5);
+console.log(sum);
+
+
+function multiplyAndDivideBy5 (num1, num2) {
+  return (num1 * num2) / 5 ;
+}
+var sum = multiplyAndDivideBy5 (35, 10);
+console.log(sum);
+
+
+function subtractTwoNumbers (num1, num2) {
+  return num1 - num2 ;
+}
+var sum = subtractTwoNumbers (22 , 7);
+console.log(sum);
+
+
+ function getCircleCircumference(radius) {
+  return  2 * Math.PI * radius
+  }
+ console.log(getCircleCircumference(5))
+
+function getFullName (firstName,lastName){
+  return firstName + lastName;
+}
+var name = getFullName("Juan ","Ramirez")
+console.log(name);
+
+function cube (number) {
+  return  Math.pow(5,3) ;
+}
+console.log(cube(5));
