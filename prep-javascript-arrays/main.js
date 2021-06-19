cf9ee2cf174ec18c4d3e4cac0d59a68fe9116c62
Jc Leon <leonjuan123@yahoo.com>
2021-06-18 22:30:31 -0700

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,19 @@
+var languages = ['html',
+'css','javascript','react','node','postgres']
+
+console.log('languages array', languages)
+
+var firstElement = languages
+
+ console.log('the first element of the array', languages[0])
+
+var thirdElement = languages
+
+ console.log('the third element of the array', languages[2])
+
+var length = languages;
+
+ console.log('the length of the array', languages.length)
+
+ var lastElement = languages
+ console.log('the last element of the array', languages[6-1]);
